8bc1b4896fe1746b9439e0dd98bc4b94315c6702
carlos pons <karlospn@gmail.com>
2024-03-29 18:56:20 +0100

    Add a clean script

--- clean.py
+++ clean.py
@@ -7,5 +7,5 @@ for dir_name in os.listdir(base_path):
     dir_path = os.path.join(base_path, dir_name)  
     if os.path.isdir(dir_path):  
         dir_time = datetime.strptime(dir_name, "%d_%m_%Y")  
-        if dir_time < datetime.now() - timedelta(days=3):  
+        if dir_time < datetime.now() - timedelta(days=7):  
             shutil.rmtree(dir_path)  
