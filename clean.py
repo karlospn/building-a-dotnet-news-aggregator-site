e52695f93d5b2120fe4f016e40b646e8a458b544
carlos pons <karlospn@gmail.com>
2024-03-29 18:55:56 +0100

    Add a clean script

---
+++ clean.py
@@ -0,0 +1,11 @@
+import os  
+from datetime import datetime, timedelta 
+import shutil 
+  
+base_path = "./site/dotnetramblings/content/post"  
+for dir_name in os.listdir(base_path):  
+    dir_path = os.path.join(base_path, dir_name)  
+    if os.path.isdir(dir_path):  
+        dir_time = datetime.strptime(dir_name, "%d_%m_%Y")  
+        if dir_time < datetime.now() - timedelta(days=3):  
+            shutil.rmtree(dir_path)  
