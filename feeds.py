5cd618ddd7d9ec8b2155c04b0c4b8350a2fa370c
carlos pons <karlospn@gmail.com>
2024-03-31 13:30:07 +0200

    print feeds

--- feeds.py
+++ feeds.py
@@ -7,7 +7,6 @@ folder_path = './data/'
 file_paths = glob.glob(os.path.join(folder_path, '*.yml'))  
 
 for file_path in file_paths:
-        print(file_path)
         with open(file_path, 'r') as f:
             data = yaml.safe_load(f)
             feeds.append(data)
@@ -17,7 +16,7 @@ with open('site/dotnetramblings/content/feeds.md', 'w') as file:
     file.write('layout: feeds\n')  
     file.write('feeds:\n')  
     for feed in feeds:          
-        
+        print(feed)
         if 'Feed' not in feed or 'Title' not in feed: 
             print(f"Missing basic data. Skipped {feed['Feed']}") 
             continue
@@ -34,6 +33,4 @@ with open('site/dotnetramblings/content/feeds.md', 'w') as file:
         if 'Language' in feed:  
             file.write(f'    language: {feed["Language"]}\n')  
     
-    file.write('---\n')
-    print(file)
- 
+    file.write('---\n') 
